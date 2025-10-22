0a5452f191eec0de5f34c7eeb63c11c00fe4bd73
Keewan <167925879+Keeuwbuck@users.noreply.github.com>
2025-10-22 13:10:24 +0200

    Add files via upload

---
+++ CrossConfig.java
@@ -0,0 +1,24 @@
+package za.ac.cput.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class CrossConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurerGlobal() {  // renamed
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
+}
